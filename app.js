8cb546c2b8a9d4fd86704b3da8be3452099b5b33
Olif51 <99129655+Olif51@users.noreply.github.com>
2022-12-15 10:35:55 +0100

    quest 4bis completed

--- app.js
+++ app.js
@@ -15,18 +15,19 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { validateMovie , validateUser } = require("./validators");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.get("/api/movies", validateMovie, movieHandlers.getMovies);
+app.get("/api/movies/:id", validateMovie, movieHandlers.getMovieById);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 
 const userHandlers = require("./userHandlers");
 
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
+app.get("/api/users", validateUser, userHandlers.getUsers);
+app.get("/api/users/:id", validateUser, userHandlers.getUserById);
+app.post("/api/users", validateUser, userHandlers.postUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
