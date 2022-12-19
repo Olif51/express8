5f88c54c728f0e37b917341a81b92659f8eafdeb
Olif51 <99129655+Olif51@users.noreply.github.com>
2022-12-19 13:52:03 +0100

    completed

--- app.js
+++ app.js
@@ -19,19 +19,30 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const { validateMovie , validateUser } = require("./validators");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+const { hashPassword, verifyPassword, verifyToken  } = require("./auth.js");
 
 const userHandlers = require("./userHandlers");
 
-const { hashPassword } = require("./auth.js");
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", validateUser, hashPassword, userHandlers.postUser);
+
+app.use(verifyToken); 
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
 
 
 app.listen(port, (err) => {
